acbcb2a4e0953e461f756797cc37e81a9a0c606b
Daniel <daniel.vernberg@gmail.com>
2016-01-15 13:45:58 +0100

    init repo

---
+++ www/js/controllers.js
@@ -0,0 +1,52 @@
+angular.module('app.controllers', [])
+
+.controller('calculateCtrl', function($scope, $http) {
+  console.log('calculateCtrl working!');
+    // Declare global variables
+    var daniel_round, caroline_round, daniel_round_percent, caroline_round_percent;
+
+  $scope.test = 'hej';
+
+  $scope.calculate = function() {
+    console.log('click');
+
+
+    // create variables that read from the input fields
+    var amount = $('#amount').val();
+    var daniel = $('#daniel').val();
+    var caroline = $('#caroline').val();
+
+    // create a total variable
+    var total = Number(amount);
+
+    // What is left after each persons own stuff is out of the picture
+    var var_av_total = (parseFloat(amount) - parseFloat(daniel) - parseFloat(caroline)) / 2;
+
+    // Total for each person
+    var daniel_total = parseFloat(daniel) + parseFloat(var_av_total);
+    var caroline_total = parseFloat(caroline) + parseFloat(var_av_total);
+
+    // Percent for each person
+    var caroline_percent = (parseFloat(caroline_total)*100/parseFloat(total));
+    var daniel_percent = (parseFloat(daniel_total)*100/parseFloat(total));
+
+    //  rounded variables for daniel. To be injected into html later
+    daniel_round = Math.round(daniel_total * 100) / 100; // variable for total kr
+    daniel_round_percent = Math.round(daniel_percent * 100) / 100; // variable for % of total
+
+    // rounded varaibles for caroline. To be injected into html later
+    caroline_round = Math.round(caroline_total * 100) / 100; // variable for total kr
+    caroline_round_percent = Math.round(caroline_percent * 100) / 100; // variable for % of total
+
+    // inject into scope
+    $scope.daniel = daniel_round;
+    $scope.caroline = caroline_round;
+    $scope.daniel_percent = daniel_round_percent;
+    $scope.caroline_percent = caroline_round_percent;
+
+  }
+})
+
+.controller('databaseCtrl', function($scope, $http) {
+  console.log('databaseCtrl working');
+})
