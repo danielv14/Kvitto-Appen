c270d7ef7ee8e69ddde25dc3252d8fc3bb5a2897
Daniel <daniel.vernberg@gmail.com>
2016-01-16 11:49:02 +0100

    using person1, person2 instead of actual names

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,47 +12,48 @@ angular.module('app.controllers', ['firebase', 'angularMoment'])
   console.log($scope.config);
 
   // Declare global variables
-  var daniel_round, caroline_round, daniel_round_percent, caroline_round_percent;
+  var person2_round, person1_round, person2_round_percent, person1_round_percent;
 
   // calculate click button
   $scope.calculate = function() {
 
     // create variables that read from the input fields
-    var amount = $('#amount').val();
-    var daniel = $('#daniel').val();
-    var caroline = $('#caroline').val();
+    var amount = $scope.amount;
+    var person1 = $scope.person1;
+    var person2 = $scope.person2;
 
     // create a total variable
     var total = Number(amount);
 
     // What is left after each persons own stuff is out of the picture
-    var var_av_total = (parseFloat(amount) - parseFloat(daniel) - parseFloat(caroline)) / 2;
+    var var_av_total = (parseFloat(amount) - parseFloat(person2) - parseFloat(person1)) / 2;
 
     // Total for each person
-    var daniel_total = parseFloat(daniel) + parseFloat(var_av_total);
-    var caroline_total = parseFloat(caroline) + parseFloat(var_av_total);
+    var person2_total = parseFloat(person2) + parseFloat(var_av_total);
+    var person1_total = parseFloat(person1) + parseFloat(var_av_total);
 
     // Percent for each person
-    var caroline_percent = (parseFloat(caroline_total)*100/parseFloat(total));
-    var daniel_percent = (parseFloat(daniel_total)*100/parseFloat(total));
+    var person1_percent = (parseFloat(person1_total)*100/parseFloat(total));
+    var person2_percent = (parseFloat(person2_total)*100/parseFloat(total));
 
-    //  rounded variables for daniel.
-    daniel_round = Math.round(daniel_total * 100) / 100; // variable for total kr
-    daniel_round_percent = Math.round(daniel_percent * 100) / 100; // variable for % of total
+    // rounded varaibles for person1.
+    person1_round = Math.round(person1_total * 100) / 100; // variable for total kr
+    person1_round_percent = Math.round(person1_percent * 100) / 100; // variable for % of total
 
-    // rounded varaibles for caroline.
-    caroline_round = Math.round(caroline_total * 100) / 100; // variable for total kr
-    caroline_round_percent = Math.round(caroline_percent * 100) / 100; // variable for % of total
+    //  rounded variables for person2.
+    person2_round = Math.round(person2_total * 100) / 100; // variable for total kr
+    person2_round_percent = Math.round(person2_percent * 100) / 100; // variable for % of total
+
 
     // inject into scope
-    $scope.daniel = daniel_round;
-    $scope.caroline = caroline_round;
-    $scope.daniel_percent = daniel_round_percent;
-    $scope.caroline_percent = caroline_round_percent;
+    $scope.person1 = person1_round;
+    $scope.person1_percent = person1_round_percent;
+    $scope.person2 = person2_round;
+    $scope.person2_percent = person2_round_percent;
 
     // Fade-in the card
-    $('#hidden-caroline-card').addClass('visible animated bounceIn');
-    $('#hidden-daniel-card').addClass('visible animated bounceIn');
+    $('#hidden-person1-card').addClass('visible animated bounceIn');
+    $('#hidden-person2-card').addClass('visible animated bounceIn');
     $('#hidden-save-button').addClass('visible animated bounceIn');
 
   }
@@ -70,8 +71,8 @@ angular.module('app.controllers', ['firebase', 'angularMoment'])
     $scope.items = Items;
     console.log($scope.items);
     $scope.items.$add({
-      costPerson1: $scope.caroline,
-      costPerson2: $scope.daniel,
+      costPerson1: $scope.person1,
+      costPerson2: $scope.person2,
       namePerson1: person1,
       namePerson2: person2,
       'createdAt': Firebase.ServerValue.TIMESTAMP,
