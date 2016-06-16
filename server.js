efe25bf8aab626ffb2155651af66352f3ceac800
Daniel <daniel.vernberg@gmail.com>
2016-06-16 11:56:22 +0200

    created express server and Procfile to be used for heroku app

---
+++ server.js
@@ -0,0 +1,16 @@
+/*
+* Simple NODE / EXPRESS server to serve static files from /www/
+* Used for Heroku
+*/
+
+var express = require('express'),
+    app = express();
+
+
+app.use(express.static('www'));
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
