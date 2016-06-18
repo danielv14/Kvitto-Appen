f72eb61fa4ae5d18261a23d648cc919a1e885c52
Daniel <daniel.vernberg@gmail.com>
2016-06-18 17:22:35 +0200

    remove not needed variables

--- www/js/controllers/calculateCtrl.js
+++ www/js/controllers/calculateCtrl.js
@@ -1,14 +1,9 @@
 app.controller('calculateCtrl', ['$scope', '$http', 'Config', 'Items', 'WhoOwesWho', 'Calculate', function($scope, $http, Config, Items, WhoOwesWho, Calculate) {
+
     // set scope variable from factories
     $scope.items = Items;
     $scope.config = Config;
     $scope.who = WhoOwesWho;
-
-    // Declare global variables
-    var person2_round, person1_round, person2_round_percent, person1_round_percent;
-
-    // variable to determine if save-button can be clicked or not
-    $scope.preventSave = true;
 
     // calculate click button
     $scope.calculate = function() {
