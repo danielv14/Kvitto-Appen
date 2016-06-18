6d03042a5f12f2d694e2f786ad6e814124de9f1b
Daniel <daniel.vernberg@gmail.com>
2016-06-18 17:14:06 +0200

    Redesign of calculation code
    Instead of doing calculations in controller app now uses a factory and
    an object to do all the calculations. The factory even attaches the
    calculations to sessionStorage upon factory return.
    Calling this factory calculate function and pass it the variable it
    needs to do calculation in the controller function for calculation page

--- www/js/controllers/calculateCtrl.js
+++ www/js/controllers/calculateCtrl.js
@@ -1,4 +1,4 @@
-app.controller('calculateCtrl', ['$scope', '$http', 'Config', 'Items', 'WhoOwesWho', function($scope, $http, Config, Items, WhoOwesWho) {
+app.controller('calculateCtrl', ['$scope', '$http', 'Config', 'Items', 'WhoOwesWho', 'Calculate', function($scope, $http, Config, Items, WhoOwesWho, Calculate) {
     // set scope variable from factories
     $scope.items = Items;
     $scope.config = Config;
@@ -17,46 +17,12 @@ app.controller('calculateCtrl', ['$scope', '$http', 'Config', 'Items', 'WhoOwesW
       var amount = $scope.amount;
       var person1 = $scope.person1;
       var person2 = $scope.person2;
+      var whoPaid = $scope.data.singleSelect;
+      var category = $scope.category.name;
 
-      // create a total variable
-      var total = Number(amount);
+      // call factory function to do calculations
+      Calculate.calculate(amount, person1, person2, whoPaid, category);
 
-      // What is left after each persons own stuff is out of the picture
-      var var_av_total = (parseFloat(amount) - parseFloat(person2) - parseFloat(person1)) / 2;
-
-      // Total for each person
-      var person2_total = parseFloat(person2) + parseFloat(var_av_total);
-      var person1_total = parseFloat(person1) + parseFloat(var_av_total);
-
-      // Percent for each person
-      var person1_percent = (parseFloat(person1_total)*100/parseFloat(total));
-      var person2_percent = (parseFloat(person2_total)*100/parseFloat(total));
-
-      // rounded varaibles for person1.
-      person1_round = Math.round(person1_total * 100) / 100; // variable for total kr
-      person1_round_percent = Math.round(person1_percent * 100) / 100; // variable for % of total
-
-      //  rounded variables for person2.
-      person2_round = Math.round(person2_total * 100) / 100; // variable for total kr
-      person2_round_percent = Math.round(person2_percent * 100) / 100; // variable for % of total
-
-
-      // inject into scope
-      $scope.person1 = person1_round;
-      $scope.person1_percent = person1_round_percent;
-      $scope.person2 = person2_round;
-      $scope.person2_percent = person2_round_percent;
-
-      // store values to sessionStorage for person 1
-      sessionStorage.costPerson1 = person1_round;
-      sessionStorage.percentagePerson1 = person1_round_percent;
-      // store values to sessionStorage for person 2
-      sessionStorage.costPerson2 = person2_round;
-      sessionStorage.percentagePerson2 = person2_round_percent;
-      // store value to sessionStorage about who paid
-      sessionStorage.whoPaid = $scope.data.singleSelect;
-      // store value to sessionStorage about receipt category
-      sessionStorage.category = $scope.category.name;
     }
 
   }])
---
+++ www/js/services/calculateService.js
@@ -0,0 +1,92 @@
+var app = angular.module('app')
+
+// factory to do calculations and set sessionStorage
+.factory("Calculate", function() {
+
+    var receipt = {
+
+      amount: '',
+      costPerson1: '',
+      costPerson2: '',
+      percentagePerson1: '',
+      percentagePerson2: '',
+      eachOfTotal: '',
+      whoPaid: '',
+      category: '',
+
+      // set total amount
+      setAmount: function(amount) {
+        this.amount = amount;
+      },
+
+      // set total cost for each person
+      // is dependant on setEachofTotal()
+      setCostPerson: function(costPerson1, costPerson2) {
+        this.costPerson1 = parseFloat(this.eachOfTotal + costPerson1);
+        this.costPerson2 = parseFloat(this.eachOfTotal + costPerson2);
+      },
+
+      // set how much cost is after each individual part is gone
+      // and it is divided by two
+      setEachofTotal: function(amount, costPerson1, costPerson2) {
+        this.eachOfTotal = (parseFloat(amount) - parseFloat(costPerson2) - parseFloat(costPerson1)) / 2;
+      },
+
+      // set who paid the receipt
+      setWhoPaid: function(whoPaid) {
+        this.whoPaid = whoPaid;
+      },
+
+      // set category of receipt
+      setCategory: function(category) {
+        this.category = category;
+      },
+
+      // set rounded percentage values for each person
+      // is dependant of setCostPerson() since it uses values set from that function
+      setRoundPercentage: function() {
+        var person1_percent = (parseFloat(this.costPerson1)*100/parseFloat(this.amount));
+        var person2_percent = (parseFloat(this.costPerson2)*100/parseFloat(this.amount));
+        this.percentagePerson1 = Math.round(person1_percent * 100) / 100;
+        this.percentagePerson2 = Math.round(person2_percent * 100) / 100;
+      }
+    }
+
+    return {
+
+      calculate: function(amount, costPerson1, costPerson2, whoPaid, category) {
+
+        // call function to set total amount
+        receipt.setAmount(amount);
+        // call function to set receipt cateogry
+        receipt.setCategory(category);
+        // call function to set who paid
+        receipt.setWhoPaid(whoPaid);
+        // call function to determine the common cost of the receipt
+        receipt.setEachofTotal(amount, costPerson1, costPerson2);
+        // call function to set individual item cost of each person
+        receipt.setCostPerson(costPerson1, costPerson2);
+        // call function to set percentage for each person
+        receipt.setRoundPercentage();
+
+        /*
+        * attach values to sessionStorage
+        */
+        // store values to sessionStorage for person 1
+        sessionStorage.costPerson1 = receipt.costPerson1;
+        sessionStorage.percentagePerson1 = receipt.percentagePerson1;
+        // store values to sessionStorage for person 2
+        sessionStorage.costPerson2 = receipt.costPerson2;
+        sessionStorage.percentagePerson2 = receipt.percentagePerson2;
+        // store value to sessionStorage about who paid
+        sessionStorage.whoPaid = receipt.whoPaid;
+        // store value to sessionStorage about receipt category
+        sessionStorage.setItem("category", receipt.category);
+
+      }
+
+    }
+
+
+
+})
