b1315e95e393b85466d2f93bd71369675221d7fa
Daniel <daniel.vernberg@gmail.com>
2016-06-21 10:11:15 +0200

    button to check if user is logged in or not

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,4 +1,19 @@
-app.controller('loginCtrl',['$scope', '$http','$firebaseArray','User', function($scope, $http, $firebaseArray, User) {
+app.controller('loginCtrl',['$scope', '$http','$firebaseArray','User', 'Auth', function($scope, $http, $firebaseArray, User, Auth) {
+
+  // setup reference variable
+  var ref = new Firebase("https://ionic-kvitto-app.firebaseio.com/");
+  var usersRef = new Firebase('https://ionic-kvitto-app.firebaseio.com/users');
+
+  $scope.auth = Auth;
+
+  // get authData
+  $scope.authDataCallback = function(authData) {
+    if (authData) {
+      console.log("User " + authData.uid + " is logged in with " + authData.provider);
+    } else {
+      console.log("User is logged out");
+    }
+  }
 
 
   // login with google
@@ -6,6 +21,16 @@ app.controller('loginCtrl',['$scope', '$http','$firebaseArray','User', function(
 
     // call user factory to authenticate with google
     User.loginGoogle();
+
+
+  }
+
+  $scope.amILoggedIn = function() {
+
+
+    // Register the callback to be fired every time auth state changes
+    ref.onAuth($scope.authDataCallback);
+
 
   }
 
