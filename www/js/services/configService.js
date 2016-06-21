e135c721af9518239008d92d922d0819212dd801
Daniel <daniel.vernberg@gmail.com>
2016-06-21 13:07:27 +0200

    comments

--- www/js/services/configService.js
+++ www/js/services/configService.js
@@ -4,6 +4,8 @@ var app = angular.module('app')
 .factory("Config", function($firebaseArray) {
 
   return {
+    // function to get and return $firebaseArray for current user
+    // determine current user by passed uid variable
     getConfigArray: function(uid) {
       var configRef = new Firebase("https://ionic-kvitto-app.firebaseio.com/users/" + uid + "/config");
       return $firebaseArray(configRef);
