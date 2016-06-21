addc88985515695edbf2e796033b058a6a65c138
Daniel <daniel.vernberg@gmail.com>
2016-06-21 15:54:11 +0200

    unpaid controller is now aware of current user

--- www/js/controllers/unpaidCtrl.js
+++ www/js/controllers/unpaidCtrl.js
@@ -1,32 +1,37 @@
 // controller for the not finished page
 app.controller('unpaidCtrl',['$scope', '$http', 'Items', 'Config', 'WhoOwesWho', 'DetermineDebt', function($scope, $http, Items, Config, WhoOwesWho, DetermineDebt) {
 
+  // get authData from current user as an object
+  var currentUser = JSON.parse(localStorage.getItem('firebase:session::ionic-kvitto-app'));
+
+  // create variable for id of current user
+  var id = currentUser.google.id;
+  
   // set up scope variables
-  $scope.notFinished = Items;
-  $scope.config = Config;
-  $scope.who = WhoOwesWho;
+  $scope.notFinished = Items.getItemsArray(currentUser.google.id);
+  $scope.config = Config.getConfigArray(currentUser.google.id);
+  $scope.who = WhoOwesWho.getDebtArray(currentUser.google.id);
   $scope.totalPerson1 = 0;
   $scope.totalPerson2 = 0;
 
   // function to mark a object as done
   $scope.markDone = function(object) {
     console.log('marking item done with id', object);
-    var itemRef = new Firebase('https://ionic-kvitto-app.firebaseio.com/receipt/' + object);
-    itemRef.update({
+    // set receipt to done
+    var receipt = new Firebase('https://ionic-kvitto-app.firebaseio.com/users/' + id + '/receipt/' + object);
+    receipt.update({
       done: true
     });
 
-    // get whoRef
-    // var whoRef = new Firebase('https://ionic-kvitto-app.firebaseio.com/who-owes-who');
-
-    // // varaibles for cost for person1 and person2
+    // varaibles for cost for person1 and person2
     var person1Cost = 0;
     var person2Cost = 0;
     var whoPayed = '';
 
     // get snapshot once
-    itemRef.once("value", function(snapshot) {
+    receipt.once("value", function(snapshot) {
       // change the variables with data from the snapshot
+      console.log(snapshot.val().whoPayed);
       person1Cost = snapshot.val().costPerson1;
       person2Cost = snapshot.val().costPerson2;
       whoPayed = snapshot.val().whoPayed;
@@ -34,24 +39,13 @@ app.controller('unpaidCtrl',['$scope', '$http', 'Items', 'Config', 'WhoOwesWho',
       console.log("The read failed: " + errorObject.code);
     });
 
+    console.log(whoPayed);
+    // call factory to determine the debt
     DetermineDebt.decreaseDebt($scope.who[0].$value, $scope.who[1].$value,
-                              whoPayed, person1Cost, person2Cost);
+                              whoPayed, person1Cost, person2Cost, id);
 
 
-    // // update who owes who values
-    // if (whoPayed == 'person1') {
-    //   tempValue = $scope.who[1].$value; // create temp-value from person 2
-    //   whoRef.update({
-    //     person2owesperson1: tempValue - person2Cost
-    //   })
-    // }
-    //
-    // if (whoPayed == 'person2') {
-    //   tempValue = $scope.who[0].$value;
-    //   whoRef.update({
-    //     person1owesperson2: tempValue - person1Cost
-    //   })
-    // }
+
 
   }
 
