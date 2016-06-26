8e4620113642b54c364240adec8fc32a61baa79b
Daniel <daniel.vernberg@gmail.com>
2016-06-26 09:26:51 +0200

    remove comment

--- www/js/services/authService.js
+++ www/js/services/authService.js
@@ -1,5 +1,5 @@
 var app = angular.module('app')
-// let's create a re-usable factory that generates the $firebaseAuth instance
+
 app.factory("Auth", ["$firebaseAuth",
   function($firebaseAuth) {
     var ref = new Firebase("https://ionic-kvitto-app.firebaseio.com");
