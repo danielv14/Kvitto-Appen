3afd68a716fed518edf11c05589b45616be5b6cb
Daniel <daniel.vernberg@gmail.com>
2016-06-30 15:56:06 +0200

    align tabs

--- www/js/controllers/calculateCtrl.js
+++ www/js/controllers/calculateCtrl.js
@@ -13,12 +13,12 @@ app.controller('calculateCtrl', ['$scope', 'Config', 'Calculate', 'User', functi
     $scope.calculate = function() {
 
       // create variables that read from the input fields
-      var amount = $scope.amount;
-      var person1 = $scope.person1;
-      var person2 = $scope.person2;
-      var whoPaid = $scope.data.singleSelect;
+      var amount   = $scope.amount;
+      var person1  = $scope.person1;
+      var person2  = $scope.person2;
+      var whoPaid  = $scope.data.singleSelect;
       var category = $scope.category.name;
-      var note = $scope.note;
+      var note     = $scope.note;
 
       // call factory function to do calculations
       Calculate.calculate(amount, person1, person2, whoPaid, category, note);
